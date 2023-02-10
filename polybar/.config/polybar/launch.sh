0c559cdb286e1f3085d23f0981aef38819e93fb6
Adrian Dinis <adriandinis99@gmail.com>
2023-02-10 09:51:20 +0000

    Remove unnecessary polybar related things

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-## Add this to your wm startup file.
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
-killall -q polybar
+killall -9 polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload main &
-done
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+  done
+else
+  polybar -q main -c "$DIR"/config.ini &
+fi
