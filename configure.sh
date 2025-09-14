81caeead7ce710f82d4c91c9879053a8e981a33b
Adrian Dinis <adriandinis99@gmail.com>
2025-09-14 01:01:16 +0100

    Goodbye old cruft

--- configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo chsh -s $(which zsh)
-cd ~/dotfiles && stow -S \
-	autorandr i3-gaps kitty nvim \
-	p10k picom polybar rofi xinit zsh \
-	&& cd -
--- nvim/.config/nvim/ftplugin/python.vim
+++
@@ -1,3 +0,0 @@
-setlocal tabstop=4 shiftwidth=4 expandtab smarttab
-let g:pyindent_open_paren = 'shiftwidth()'
-let g:pyindent_continue = 'shiftwidth()'
