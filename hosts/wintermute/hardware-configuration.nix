01bdaa5bfca93f4a6560979c634f6bbe71816361
Adrian Dinis <adriandinis99@gmail.com>
2025-09-16 23:04:30 +0100

    introducing this back

--- hosts/wintermute/hardware-configuration.nix
+++ hosts/wintermute/hardware-configuration.nix
@@ -1,1 +1,9 @@
-{ config, pkgs, lib, ... }: { }
+{ config, pkgs, lib, ... }: {
+  # Disko will handle most filesystem configuration
+  # We only need to specify neededForBoot for persistence filesystems
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/var/log".neededForBoot = true;
+  fileSystems."/secure".neededForBoot = true;
+  fileSystems."/secure/ssh".neededForBoot = true;
+  fileSystems."/secure/secrets".neededForBoot = true;
+}
