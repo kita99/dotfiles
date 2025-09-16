456ca375e6f9683b2627ddc8639a7e8d992f3c1b
Adrian Dinis <adriandinis99@gmail.com>
2025-09-16 21:52:05 +0100

    add some thing

--- hosts/wintermute/configuration.nix
+++ hosts/wintermute/configuration.nix
@@ -1,13 +1,18 @@
 { config, pkgs, lib, ... }:
 
 {
-  networking.hostName = "kita";
+  networking.hostName = "wintermute";
+
+  users.mutableUsers = false;
 
   users.users.kita = {
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTM2+UKKWOSpaP0tW2+Aq/KLtn2tYjkQWLMUVZoltB8AV3KMder8y2cBDMqIhQ2pQopgqjYV71AKJgrBRgtq1Kc2ypTIlc2dXdZiSa3S06hdRd0ln2AQpMofNDO/ZfpnErjM/p1/DknuDPXc50hXMafdyHDob/oQhvTIPysIXqSRinZPM1DY4GFZfrZYcqd8Iep9v0NLJiULBiODJd89+mMGcvjNDTXB+9BLx5VlWW4myGfacWxFTj+HEezI3EVRevBlWeMIflLs0EriTmYz28b5+rODpGXlaXW4H0Fj90g0k9OwgbIeuZvANcG4GidP6IueJDcQExlRh0betmlH7mJYpV86pazhAIzq5W7xNhTaOPoAkOBmBmQRfH/WuKhO6rILZ4HqkUzSkyLmIYYyweQ5YCz6y+6lAA6iSxmVfLStGHQv3wvPxhUtLM63IgfsJojO+2fPW8Igj9dGgwvVyzF3/bSRDxhDACIs0F81Mb2jkORgMQmu7vnZRRPAGfTKU="
+    ];
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     home = "/home/kita";
-    password = ""; # set after install
+    initialPassword = "changeme";
   };
 
   services.openssh.enable = true;
@@ -28,5 +33,5 @@
     };
   };
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "25.05";
 }
