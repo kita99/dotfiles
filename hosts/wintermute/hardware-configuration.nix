bf65a740523b1ff2be0c91e7a110c8cad6fa074d
Adrian Dinis <adriandinis99@gmail.com>
2025-10-25 10:10:13 +0100

    maybemaybemaybe

--- hosts/wintermute/hardware-configuration.nix
+++ hosts/wintermute/hardware-configuration.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }: {
   # Disko will handle most filesystem configuration
   # We only need to specify neededForBoot for persistence filesystems
+  fileSystems."/".neededForBoot = true;
+  fileSystems."/nix".neededForBoot = true;
   fileSystems."/persist".neededForBoot = true;
   fileSystems."/var/log".neededForBoot = true;
   fileSystems."/secure".neededForBoot = true;
