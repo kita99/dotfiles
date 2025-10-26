83b6dfc6f54855ce41d282f1192377e4f65f8960
Adrian Dinis <adriandinis99@gmail.com>
2025-10-26 23:07:43 +0000

    -.-

--- hosts/wintermute/configuration.nix
+++ hosts/wintermute/configuration.nix
@@ -22,7 +22,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # Enable systemd in initrd for better handling of mount dependencies
   boot.initrd.systemd.enable = true;
